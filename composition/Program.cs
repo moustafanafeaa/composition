4911165b9add3e58d78fb938b30d6bfeb103c316
moustafanafeaa <moustafaahmednafea@gmail.com>
2023-09-16 04:11:27 +0300

    Add project files.

---
+++ composition/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using inheritance;
+
+var choice = 0;
+do
+{
+    choice = readchoice(choice);
+    if(choice >=1 && choice <= 2)
+    {
+        var e = choose(choice);
+        Console.WriteLine(e);
+        Console.WriteLine("press any key");
+    }
+    Console.ReadKey();
+       
+
+}while (choice != 0);
+
+static int readchoice(int choice)
+{
+    Console.WriteLine("which one");
+    Console.WriteLine("------------");
+    Console.WriteLine("1-overtime");
+    Console.WriteLine("2-Residence");
+    if (int.TryParse(Console.ReadLine(), out int ch))
+    {
+        choice = ch;
+    }
+
+
+    return choice;
+}
+static employees choose(int choice)
+{
+    employees e = null;
+    switch (choice)
+    {
+        case 1:
+            e = new overtime();
+            break;
+        case 2:
+            e = new Residence();
+            break;
+        default:
+            break;
+    }
+    return e;
+}
---
+++ composition/employees.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    public class employees
+    {
+        public virtual int salary => 1000;
+        public virtual string Bonus => "none";
+        
+        public List<Ibonus> bouns { get; private set; } = new List<Ibonus>();
+        
+        public override string ToString()
+        {
+            return $"{Bonus}\n" +
+                $"total salary = {salary}";
+        }
+
+    }
+
+    public class Residence : Ibonus
+    {
+        public  string Bonus => "Residence";
+        public int salary => 1000;
+
+    }
+    public class overtime : Ibonus
+    {
+        public  int salary => 200;
+        public  string Bonus => $"overtime {2}h";
+        
+    }
+    interface Ibonus
+    {
+        int salary {get;}
+        string Bonus { get;}
+    }
+}
